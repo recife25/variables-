c4404a3d7db5411d679ce353ef8d502bfae1e278
recife25 <felipe.j.ruiz64@gmail.com>
2020-10-29 15:59:42 -0400

    added variables file with interpolation

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Felipe Ruiz
+//variables using interpolation and console.writeline(); 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//declared and initialized a viriable 
+			//camelCase for the variable name 
+			//explicit typing
+
+
+			string dogName = "Ralf";
+			int dogAge = 10;
+			char nickName = 'R';
+			bool lovesHiking = true;
+			double tennisBalls = 32;
+			decimal weight = 70.3m;
+
+			//string interpolation with the $ and {} around variables
+			Console.WriteLine($"MY dog's name is {dogName}, He is {dogAge} years old and has {tennisBalls} tennis balls and it is {lovesHiking} that he loves to hike and he is {weight} pounds in weight");
+
+
+
+		}
+	}
+}
