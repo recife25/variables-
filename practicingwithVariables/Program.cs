0d6902becd110f4012f377aabac40a3713f5efe9
recife25 <felipe.j.ruiz64@gmail.com>
2020-10-29 17:08:15 -0400

    added practicewithVariables

---
+++ practicingwithVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿//felipe Ruiz
+//Practicewithvariables
+using System;
+
+namespace PracticeWithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // int? it makes datetype nullable
+            int number = 5;
+            bool conditional = true;
+            char letter = 'N';
+            //camel case 
+            string firstName = "Felipe";
+            double myDouble = 56.32;
+            decimal myDecimal = 56.32M;
+            float myFloat = 34.32f;
+
